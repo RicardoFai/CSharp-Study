77f50006a848d57c31f5bd6840f4af86b931d955
Ricardo da Silva Gomes <ricardofai03@gmail.com>
2026-01-23 11:26:18 -0300

    Random

---
+++ M01A09/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random gerador = new Random();
+int n = gerador.Next(1, 100);
+
+Console.WriteLine("Acabei de gerar um número " + n);
