016a71ef469b9edae14968c3898d3f0d4cb843b0
Ricardo da Silva Gomes <ricardofai03@gmail.com>
2026-01-24 12:12:26 -0300

    ex 10

---
+++ M01A10/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int dia = DateTime.Now.Day;
+int mes = DateTime.Now.Month;  
+int ano = DateTime.Now.Year;
+Console.WriteLine("A data atual é dia :  "  + dia +  " do mês "  + mes +  " do ano "  + ano);
+
+
+int hora = DateTime.Now.Hour;
+int minuto = DateTime.Now.Minute;
+int segundo = DateTime.Now.Second;
+Console.WriteLine("A hora atual é : "  + hora +  " horas, "  + minuto +  " minutos e "  + segundo +  " segundos.");
