619183285d610783088d8ef21bd89f7587fecbf5
Ricardo da Silva Gomes <ricardofai03@gmail.com>
2026-01-26 15:44:46 -0300

    M01Aula13

---
+++ M01A13A/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+string nome = "Ricardo";
+Console.WriteLine($"Muito prazer em te conhece, { nome}!!! ");
---
+++ M01A13A1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string nome = "Ricardo";
+float sal= 550.75644f;
+int idade = 22;
+Console.WriteLine($"O {nome, -10} tem {idade, -5:D} ganha {sal:C2} por mês.");
+int num = 13;
+Console.WriteLine($"O número foi {num:x}");
+float val = 44679.003f;
+Console.WriteLine($"O valor foi {val:E}");
