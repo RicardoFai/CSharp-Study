e1b99f22059217d0e343aa40fadf129f218f0be9
Ricardo da Silva Gomes <ricardofai03@gmail.com>
2026-01-28 12:24:59 -0300

    ex003

---
+++ ex003/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite um número: ");
+float num;
+float.TryParse(Console.ReadLine(), out num);
+
+int n1 = (int)num;
+int n2 = Convert.ToInt16(num);
+
+Console.Clear();
+Console.WriteLine($"Você digitou o valor {num:N1}");
+Console.WriteLine($"A parte inteira do número é {n1:D}");
+Console.WriteLine($"Arrendodando, temos o número {n2}");
+
