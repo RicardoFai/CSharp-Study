86ee37d9a612f9cba5531513a843d1ecb8d75554
Ricardo da Silva Gomes <ricardofai03@gmail.com>
2026-01-29 12:01:28 -0300

    ex 005 e 006

---
+++ Ex005/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Em que ano você nasceu?");
+int anoNascimento;
+int.TryParse(Console.ReadLine(), out anoNascimento);
+int idade = 2026 - anoNascimento;
+Console.WriteLine("-------------------------------------------------------------------");
+Console.WriteLine($"Quem nasceu em {anoNascimento} vai completar {idade} anos em 2026.");
