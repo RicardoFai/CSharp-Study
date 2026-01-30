98bf81b86fec26e9e1e6dce46280e1f3f254d7bf
Ricardo da Silva Gomes <ricardofai03@gmail.com>
2026-01-30 11:33:28 -0300

    ex 007

---
+++ ex007/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int ini, fim;
+Console.WriteLine("\nSORTEADOR DE NÚMEROS");
+Console.WriteLine("-----------------------------");
+Console.Write("Início: ");
+int.TryParse(Console.ReadLine(), out ini);
+Console.Write("Fim: ");
+int.TryParse(Console.ReadLine(), out fim);
+
+Console.WriteLine("-----------------------------");
+Console.SetCursorPosition(0, 6);
+Console.Write("Sorteando...");
+Thread.Sleep(2000);
+Random gerador = new Random();
+int num = gerador.Next(ini, fim + 1);
+Console.SetCursorPosition(0, 6);
+Console.Write($"Entre {ini} e {fim} sorteei o valor {num}");
+
+
+
---
+++ ex008/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ex008
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponet();
+        
+            }
+
+        private void btnSorteio_Click(object sender, RoutedEventArgs e)
+        {
+            int ini, fim;
+            int.TryParse(txtIni.Text, out ini);
+            int.TryParse(txtFim.Text, out fim);
+            
+            
+            Random gerador = new Random();
+            int num = gerador.Next(ini, fim + 1);
+
+            lblSorteio.Content = $"Sorteei o valor {num}";
+
+        }
+    }
+}
