2b6c492e202f6e4095580615a08c67b3c86017df
Ricardo da Silva Gomes <ricardofai03@gmail.com>
2026-01-30 12:08:22 -0300

    ex008

---
+++ ex008/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ex008
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private async void btnSortear_Click(object sender, RoutedEventArgs e)
+        {
+            int ini, fim;
+            int.TryParse(txtIni.Text, out ini);
+            int.TryParse(txtFim.Text, out fim);
+
+            Random gerador = new Random();
+            int num = gerador.Next(ini, fim + 1);
+            lblSorteio.Visibility = Visibility.Visible;
+            lblSorteio.Content = $"Entre {ini} e {fim} sorteei o valor ";
+            await Task.Delay(2000);
+            lblSorteio.Content = $"Sorteei o valor {num}";
+
+            
+        }
+    }
+}
