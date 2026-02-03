1a30692ac3ed4bdfb5cd9d4837b8cd69e94728fb
Ricardo da Silva Gomes <ricardofai03@gmail.com>
2026-02-03 11:48:13 -0300

    M02ex01

---
+++ m02ex01/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Exemplo de operadores aritméticos
+
+int op1 = 0;
+int op2 = 0;
+
+
+Console.Write("Digite o primeiro operando: ");
+int.TryParse(Console.ReadLine(), out op1);
+Console.Write("Digite o segundo operando: ");
+int.TryParse(Console.ReadLine(), out op2);
+
+Console.WriteLine($"Calculando +{op1} = {+op1}"); // identidade
+Console.WriteLine($"Calculando -{op1} = {-op1}"); // negação
+Console.WriteLine($"Calculando {op1} + {op2} = {op1 + op2} "); // adição
+Console.WriteLine($"Calculando {op1} - {op2} = {op1 - op2} ");// subtração
+Console.WriteLine($"Calculando {op1} * {op2} = {op1 * op2} ");// multiplicação
+Console.WriteLine($"Calculando {op1} / {op2} = {op1 / op2} **ERRO** ");// divisão inteira
+Console.WriteLine($"Calculando {op1} % {op2} = {op1 % op2} ");// resto da divisão
+
+
+
