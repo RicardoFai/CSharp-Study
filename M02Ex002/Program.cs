b6cc36763b7b75ca030f801df49a7f28df155c22
Ricardo da Silva Gomes <ricardofai03@gmail.com>
2026-02-04 11:23:28 -0300

    M02EX002

---
+++ M02Ex002/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n1 = 0;
+int n2 = 0;
+
+Console.Write("Numerador: ");
+int.TryParse(Console.ReadLine(), out n1);
+Console.Write("Denominador: ");
+int.TryParse(Console.ReadLine(), out n2);
+
+
+
+Console.WriteLine($"Divisão inteira {n1} / {n2} = {(int)n1 / (int)n2:D}");
+Console.WriteLine($"Divisão reL DE {n1} / {n2} = {(float)n1 / (float)n2:F2}");
+
+
+
