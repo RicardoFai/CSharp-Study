4f6adc6759f9a7cff06b41408ed2499fc4b03963
Ricardo da Silva Gomes <ricardofai03@gmail.com>
2026-02-22 12:17:24 -0300

    M02ex004

---
+++ M02ex004/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more informatio//
+int n1 = 0;
+int n2 = 0;
+Console.Write("Digite um número: ");
+int.TryParse(Console.ReadLine(), out n1);
+Console.Write("Digite outro número: ");
+int.TryParse(Console.ReadLine(), out n2);
+Console.WriteLine($"{n1} == {n2} ? {n1 == n2}");
+Console.WriteLine($"{ n1} != { n2} ? {n1 != n2}");
+Console.WriteLine($"{n1} > {n2} ? {n1 > n2}");
+Console.WriteLine($"{n1} < {n2} ? {n1 < n2}");
+Console.WriteLine($"{n1} >= {n2} ? {n1 >= n2}");
+Console.WriteLine($"{n1} <= {n2} ? {n1 <= n2}");
+Console.WriteLine($"{n1} é inteiro ? {n1 is int}");
+
+
