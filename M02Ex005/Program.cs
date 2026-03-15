f4725aa26024aa1eb392b2db1c454be547d42014
Ricardo da Silva Gomes <ricardofai03@gmail.com>
2026-03-14 22:03:19 -0300

    M02ex005

---
+++ M02Ex005/Program.cs
@@ -0,0 +1,7 @@
+﻿bool p = true;
+bool q = true;
+
+
+Console.WriteLine($"não {p} = {!p}"); // Operador lógico de negação
+Console.WriteLine($"{p} e {q} = {p && q}"); // Operador lógico de conjunção (AND) - retorna true se ambos os operandos forem true
+Console.WriteLine($"{p} ou {q} = {p || q}");// Operador lógico de disjunção (OR) - retorna true se pelo menos um dos operandos for true
